8984638d802d02fb655882e6852cd32f3f454451
FelipeVieira4 <felipevieira58238@gmail.com>
2024-01-15 13:26:18 -0300

    fix: alterado @NotBlank para @Positive

--- src/main/java/com/banco/backend/dots/ContaDots.java
+++ src/main/java/com/banco/backend/dots/ContaDots.java
@@ -1,6 +1,6 @@
 package com.banco.backend.dots;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record ContaDots(@NotNull @NotBlank int clienteID, float saldo) {}
+public record ContaDots(@NotNull @Positive int clienteID, @NotNull float saldo) {}
