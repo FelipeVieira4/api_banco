b793352139f4c77d20c4afc08064d2fdb0b8e7db
FelipeVieira4 <felipevieira58238@gmail.com>
2024-01-15 13:25:53 -0300

    update: add método findById

--- src/main/java/com/banco/backend/repository/ContaRepository.java
+++ src/main/java/com/banco/backend/repository/ContaRepository.java
@@ -1,8 +1,11 @@
 package com.banco.backend.repository;
 
+import com.banco.backend.models.ClienteModel;
 import com.banco.backend.models.ContaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ContaRepository extends JpaRepository<ContaModel, Integer> {
-    
+    public Optional<ContaModel> findById(int id);
 }
